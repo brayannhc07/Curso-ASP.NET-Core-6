4602d968bf180b36b6bfdec19ee8a4a7971dc2b5
Brayan Hernández <brayannhc07@gmail.com>
2022-09-02 20:43:13 -0500

    Sección 4: Fin de la sección (Primer Api con EF6)

---
+++ WebApiAutores/WebApiAutores/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace WebApiAutores {
+	public class Startup {
+		public Startup(IConfiguration configuration) {
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+		public void ConfigureServices( IServiceCollection services ) {
+
+			// Add services to the container.
+
+			services.AddControllers().AddJsonOptions( x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+			services.AddDbContext<ApplicationDbContext>( options => options.UseSqlServer( Configuration.GetConnectionString( "DefaultConnection" ) ) );
+
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+		}
+
+		public void Configure( IApplicationBuilder app, IWebHostEnvironment env ) {
+			// Configure the HTTP request pipeline.
+			if( env.IsDevelopment() ) {
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints => {
+				endpoints.MapControllers();
+			} );
+		}
+	}
+}
